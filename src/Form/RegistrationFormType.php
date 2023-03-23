facbd05ba003f66f9bec6702f8a1266c215ff563
Augustin <augustinvathonne@gmail.com>
2023-03-23 09:21:12 +0100

    Ajout de l'entity réservation et de ses propriétés

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,47 +5,110 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email',EmailType::class,[
+                'attr' => [
+                    'placeholder' => 'Votre email ',
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('lastname',TextType::class,[
+                
+                'attr' => [
+                    'placeholder' => 'Votre nom ',
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('firstname',TextType::class,[
+                
+                'attr' => [
+                    'placeholder' => 'Votre prénom ',
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('password', RepeatedType::class, [
+                'label' => false,
+                'type' => PasswordType::class,
+                // 'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password',],
+                'first_options'  => ['label' => 'Password',
+                                     'attr'=> [
+                                        'class' => 'form-control'
+                                     ]
+                                     ],
+                'second_options' => ['label' => 'Repeat Password',
+                'attr'=> [
+                   'class' => 'form-control'
+                ]],
+                
+            ])
+            ->add('address',TextType::class,[
+                
+                'attr' => [
+                    'placeholder' => 'Votre adresse ',
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('city',TextType::class,[
+                
+                'attr' => [
+                    'placeholder' => 'Votre ville ',
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('zipscode',IntegerType::class,[
+                
+                'attr' => [
+                    'placeholder' => 'Votre code postal ',
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('country',CountryType::class,[
+                
+                'attr' => [
+                    'placeholder' => 'Votre pays ',
+                    'class' => 'form-control'
+                ]
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
+                'attr' =>[
+                    'class' => 'form-check-input'
+                ],
                 'constraints' => [
                     new IsTrue([
                         'message' => 'You should agree to our terms.',
                     ]),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
-            ])
+            ->add('sumbit', SubmitType::class,[
+                'label' => 'S\'inscrire',
+                'attr' => [
+    
+                    'class' => 'btn btn-primary'
+                    ]
+                ]);
         ;
     }
+    
 
     public function configureOptions(OptionsResolver $resolver): void
     {
