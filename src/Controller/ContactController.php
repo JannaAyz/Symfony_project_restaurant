58164510a3c2270f4acf700a1a6865db5d543698
Janna Ayz <jansary@yahoo.fr>
2023-03-27 10:07:10 +0200

    contact page with redirection and modal when message sent

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
+use Symfony\Component\HttpFoundation\Cookie;
 
 class ContactController extends AbstractController
 {
@@ -28,9 +29,29 @@ class ContactController extends AbstractController
             ->to('you@example.com')
             ->text($message);
             //->html('<p>See Twig integration for better HTML integration!</p>');
+             
+            // $mailer->send($email);
+            // $response = $this->redirectToRoute('app_home');
+            // $response->headers->setCookie(new Cookie('launch', 'true'));
 
-            $mailer->send($email);
-            return $this->redirectToRoute('app_home');
+            echo '<script>setTimeout(() => {
+                launchModal();
+              }, "300");</script>';
+            sleep(6);
+            $response = $this->redirectToRoute('app_home');
+            
+            // Définir la date d'expiration du cookie
+            $expireDate = (new \DateTimeImmutable())->modify('+1 minute');
+
+            // Créer un nouveau cookie avec la date d'expiration
+            $response->headers->setCookie(new Cookie('launch', 'true', $expireDate)); 
+            return $response;
+            // $home = (new HomeController());
+            // $home->launchModal(); 
+
+
+            // return $this->redirectToRoute('app_home', ['show_modal' => true]);
+            // return $this->redirect($request->getUri());
         }
 
         return $this->renderForm('contact/index.html.twig', [
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,46 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Cookie;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $cookies = $request->cookies;
+        if ($cookies->has('launch')) {
+            // Exécutez la fonction "launchTruc()"
+            
+            // Attendre 5 secondes
+        }
+    
+
+    
+        //     // Supprimez le cookie pour qu'il ne soit pas exécuté à chaque visite
+        
+            
+            
+        // }
+
+        // $launch = $request->query->get('launch');
+
+        
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
+    
+    sleep(5);
+    $response = new Response();
+    $response->headers->clearCookie('launch');
+
+    return $response;
+            // $response->headers->setCookie(new Cookie('launch', 'false'));
+               
+            
+            
+            
+        // return $response; 
     }
 }
