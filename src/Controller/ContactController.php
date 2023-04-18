e2baee64e98adae1a7e69a1d6d981964ba83c6b1
JannaAyz <jansary@yahoo.fr>
2023-04-18 10:31:14 +0200

    more CSS and JS on home page

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -35,13 +35,17 @@ class ContactController extends AbstractController
             echo '<script>setTimeout(() => {launchModal();}, "300");</script>';
             //so that the modal has time to appear
             sleep(6);
+            $truc = $this->renderView('partials/modal.html.twig');
             $response = $this->redirectToRoute('app_home');
             return $response;
+        }else{
+            $truc = null;
         }
 
         return $this->renderForm('contact/index.html.twig', [
             'controller_name' => 'ContactController',
-            'formulaire' => $form
+            'formulaire' => $form,
+            'modal' => $truc // Inclure le fichier modal.html.twig dans la réponse Twig
         ]);
     }
 
